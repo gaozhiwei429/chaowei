ec2dc65efa243c0e744e11c1ccfee1e3c1dba154
yuezl <yuezl1993@163.com>
2018-09-04 17:53:03 +0800

    commt

--- Component/AboutUs/AboutUs.js
+++ Component/AboutUs/AboutUs.js
@@ -32,7 +32,7 @@ export default class CWButton extends Component {
         this.state = {
             version: '',
             phoneNumber:''
-        }
+        } 
     }
 
     componentDidMount(){
